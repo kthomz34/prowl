75f6e6e94beacef7f12f489334e78b734623c620
Kurt <kurt.thomas34@gmail.com>
2017-05-07 15:31:17 -0400

    added intent of checking cities

--- lambda/index.js
+++ lambda/index.js
@@ -22,6 +22,41 @@ var handlers = {
   'AlexaMeetUpNumbers': function () {
     var meetupNumbers = alexaMeetups.length;
     this.emit(':ask', `I currently know of ${meetupNumbers} Alexa developer meetups. Check to see if your city is one of them!`, 'How can I help?');
+  },
+
+  'AlexaMeetupCityCheck': function () {
+    // Get Slot Values
+    var USCitySlot = this.event.request.intent.slots.USCity.value;
+    var EuropeanCitySlot = this.event.request.intent.slots.EuropeanCity.value;
+
+    // Get city
+    var city;
+    if (USCitySlot) {
+      city = USCitySlot;
+    }
+    else if (EuropeanCitySlot) {
+      city = EuropeanCitySlot;
+    }
+    else {
+      this.emit(':ask', 'Sorry, I didn\'t recognise that city name.', 'How can I help?');
+    }
+
+    // Check for our city
+    var cityMatch = '';
+    for (var i = 0; i < alexaMeetups.length; i++) {
+      if (alexaMeetups[i].city.toLowerCase() === city.toLowerCase()){
+        cityMatch = alexaMeetups[i].city;
+      }
+    }
+
+    // Respond to User
+    if (cityMatch !== '') {
+      this.emit(':ask', `Yes! ${city} has an Alexa developer meetup!`, 'How can I help?');
+    }
+    else {
+      this.emit(':ask', `Sorry, looks like ${city} doesn't have an Alexa developer meetup yet - why don't you start one?`, 'How can I help?');
+    }
+    
   }
 
 };
