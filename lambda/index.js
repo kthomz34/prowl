0ad594ec6de86988f187595f5f53ae5b82463aa4
Kurt <kurt.thomas34@gmail.com>
2017-05-07 00:31:18 -0400

    added alexa meetup intent

--- lambda/index.js
+++ lambda/index.js
@@ -1,5 +1,8 @@
 var Alexa = require('alexa-sdk');
 
+// Data
+var alexaMeetups = require('./data/alexaMeetups');
+
 exports.handler = function(event, context, callback){
   var alexa = Alexa.handler(event, context);
   alexa.registerHandlers(handlers);
@@ -14,6 +17,11 @@ var handlers = {
 
   'Hello': function () {
     this.emit(':tell', 'Hi there!');
+  },
+
+  'AlexaMeetUpNumbers': function () {
+    var meetupNumbers = alexaMeetups.length;
+    this.emit(':ask', `I currently know of ${meetupNumbers} Alexa developer meetups. Check to see if your city is one of them!`, 'How can I help?');
   }
 
 };
