df2c0b77dba33561f838855309dc90ed73a538d5
Kurt <kurt.thomas34@gmail.com>
2017-05-11 18:31:49 -0400

    able to pull name info from meetup api now

--- lambda/handlers/onboardingStateHandlers.js
+++ lambda/handlers/onboardingStateHandlers.js
@@ -3,6 +3,9 @@ var Alexa = require('alexa-sdk');
 // Constants
 var constants =  require('../constants/constants');
 
+// Helpers
+var meetupAPI = require('../helpers/meetupAPI');
+
 // Onboarding Handlers
 var onboardingStateHandlers = Alexa.CreateStateHandler(constants.states.ONBOARDING, {
 
@@ -10,56 +13,42 @@ var onboardingStateHandlers = Alexa.CreateStateHandler(constants.states.ONBOARDI
     // Check for User Data in Session Attributes
     var userName = this.attributes['userName'];
     if (userName) {
-
       // Change State to Main
       this.handler.state = constants.states.MAIN;
       this.emitWithState('LaunchRequest');
-
     } else {
-      this.emit(':ask', 'Welcome to Group Finder, The skill that gives you information about the alexa developer community. You can ask me about the various alexa meetups around the world. But first, I\'d like to get to know you better. Tell me your name by saying: My name is, and then your name.', 'Tell me your name by saying: My name is, and then your name.');
+      // Get Access Token
+      var accessToken = this.event.session.user.accessToken;
+
+      // Account Linked
+      if (accessToken) {
+
+        // Get User Details from Meetup API
+        meetupAPI.GetUserDetails(accessToken)
+          .then((userDetails) => {
+            console.log('userDetails', JSON.stringify(userDetails));
+            // Get Users Name
+            var name = userDetails.name;
+
+            // Store Users Name in Session
+            this.attributes['userName'] = name;
+
+            // Change State to MAIN
+            this.handler.state = contstants.states.MAIN;
+
+            // Welcome User for the First Time
+            this.emit(':ask', `Hi ${name}! Welcome to Group Finder! The Skill that gives you all the information about the alexa developer community. You can ask me about the various alexa meetups around the world. What would you like me to do?`, 'What would you like me to do?');
+          })
+          .catch((error) => {
+            console.log('Meetup API ERROR', error);
+            this.emit(':tell', `Sorry, there was a problem accessing your meetup account details.`);
+          });
+      } else {
+        // Account Not Linked
+        this.emit(':tellWithLinkAccountCard', `Please link your account to use this skill. I\'ve sent the details to your alexa account.`);
+      }
     }
-  },
 
-  'NameCapture': function () {
-    // Get Slot Values
-    var USFirstNameSlot = this.event.request.intent.slots.USFirstName.value;
-    var UKFirstNameSlot = this.event.request.intent.slots.UKFirstName.value;
-
-    // Get Name
-    var name;
-    if (USFirstNameSlot) {
-      name = USFirstNameSlot;
-    } else if (UKFirstNameSlot) {
-      name = UKFirstNameSlot;
-    }
-
-    // Save Name in Session Attributes and Ask For Country
-    if (name) {
-      this.attributes['userName'] = name;
-      this.emit(':ask', `Ok, ${name}! Tell me what country you're from by saying: I'm from, and then the country you're from.`, 'Tell me what country you\'re from by saying: I\'m from, and then the country you\'re from.');
-    } else {
-      this.emit(':ask', `Sorry, I didn\'t recognise that name!`, `Pleae tell me your name by saying: My name is, and then your name.`, 'Pleae tell me your name by saying: My name is, and then your name.');
-    }
-  },
-
-  'CountryCapture': function () {
-    //Get Slot Values
-    var country = this.event.request.intent.slots.Country.value;
-
-    // Get User Name from Session attributes
-    var userName = this.attributes['userName'];
-
-    // Save Country Name in Session Attributes
-    if (country) {
-
-      this.attributes['userCountry'] = country;
-
-      // Change the state to Main
-      this.handler.state = constants.state.MAIN;
-      this.emit(':ask', `Ok ${userName}! Your from ${country}, that's great! You can ask me about the various alexa meetups around the world. What would you like to do?`, 'What would you like to do?');
-    } else {
-      this.emit(':ask', `Sorry, I didn\'t recognise that country!`, `Please tell me what country you\'re from by saying: I\'m from, and then the country you\'re from.`);
-    }
   },
 
   'AMAZON.StopIntent': function () {
@@ -78,17 +67,12 @@ var onboardingStateHandlers = Alexa.CreateStateHandler(constants.states.ONBOARDI
   },
 
   'AMAZON.HelpIntent': function () {
-    // User Name Attribute Check
-    var userName = this.attributes['userName'];
-
-    if (userName) {
-      this.emit(':ask', `Please tell me what country you're from by saying: I'm from, and then the country you're from.`, 'Tell me what country you\'re from by saying: I\'m from, and then the country you\'re from.');
-    } else {
-      this.emit(':ask', `Please tell me your name by saying: My name is, and then your name.', 'Tell me your name by saying: My name is, and then your name.`);
-    }
+    // Account not Linked
+    this.emit(':tellWithLinkAccountCard', `Please link your account to use this skill. I\'ve sent the details to your alexa account.`);
   },
 
   'Unhandled': function () {
+    // Account not Linked
     this.emitWithState('AMAZON.HelpIntent');
   }
 
